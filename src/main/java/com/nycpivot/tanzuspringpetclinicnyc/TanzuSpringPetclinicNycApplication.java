798bb78b87dbd16ab2c035798130f90a446f527a
mijames <mijames@vmware.com>
2022-01-28 14:47:02 -0500

    Initial commit.

---
+++ src/main/java/com/nycpivot/tanzuspringpetclinicnyc/TanzuSpringPetclinicNycApplication.java
@@ -0,0 +1,13 @@
+package com.nycpivot.tanzuspringpetclinicnyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanzuSpringPetclinicNycApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TanzuSpringPetclinicNycApplication.class, args);
+    }
+
+}
